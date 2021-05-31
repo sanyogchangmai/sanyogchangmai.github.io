c6bbaa5504a0cecd64089ca9f32f2d213ea2470b
sanyogchangmai <sanyogchangmai29@gmail.com>
2021-05-31 13:27:20 +0530

    initial commit

--- javascript/sidenav.js
+++ javascript/sidenav.js
@@ -4,17 +4,20 @@ $(document).ready(function(){
     $(".hemburg").click(function(){
         $(".side-nav").toggleClass("side-nav-hide");
         $(".overlay").toggleClass("overlay-hide");
+        $(".btn-floating").addClass("btn-floating-hide");
 
     });
 
     $(".overlay").click(function(){
         $(".overlay").toggleClass("overlay-hide");
         $(".side-nav").toggleClass("side-nav-hide");
+        $(".btn-floating").removeClass("btn-floating-hide");
     });
 
     $(".side-link").click(function(){
         $(".overlay").toggleClass("overlay-hide");
         $(".side-nav").toggleClass("side-nav-hide");
+        $(".btn-floating").removeClass("btn-floating-hide");
     });
 
 });
