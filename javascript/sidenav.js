91ba0ee9f47878c23ea397fb6194bf96232c15f5
sanyogchangmai <sanyogchangmai29@gmail.com>
2021-06-01 17:01:15 +0530

    add side-nav animation

--- javascript/sidenav.js
+++ javascript/sidenav.js
@@ -2,21 +2,24 @@
 $(document).ready(function(){
 
     $(".hemburg").click(function(){
-        $(".side-nav").toggleClass("side-nav-hide");
-        $(".overlay").toggleClass("overlay-hide");
+        $(".side-nav").removeClass("side-nav-hide").animate({left:"80%"},"300");
+        $(".overlay").removeClass("overlay-hide");
         $(".btn-floating").addClass("btn-floating-hide");
 
     });
 
     $(".overlay").click(function(){
-        $(".overlay").toggleClass("overlay-hide");
-        $(".side-nav").toggleClass("side-nav-hide");
+        
+        // $(".side-nav").addClass("side-nav-hide");
+        $(".overlay").addClass("overlay-hide");
+        $(".side-nav").animate({left:"-80%"},"slow");
+        
         $(".btn-floating").removeClass("btn-floating-hide");
     });
 
     $(".side-link").click(function(){
         $(".overlay").toggleClass("overlay-hide");
-        $(".side-nav").toggleClass("side-nav-hide");
+        $(".side-nav").toggleClass("side-nav-hide").animate({left:"-80%"},"300");
         $(".btn-floating").removeClass("btn-floating-hide");
     });
 
